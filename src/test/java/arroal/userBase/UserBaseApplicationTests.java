b0a57619658bce673ed86d93d637a5ef36874ae4
roman.arzamastsev <roman.arzamastsev@russianpost.ru>
2023-01-04 15:55:00 +0300

    Initial commit

---
+++ src/test/java/arroal/userBase/UserBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package arroal.userBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
