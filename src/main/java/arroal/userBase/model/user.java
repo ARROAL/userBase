c8e8b525d117e918d4c0134bb34efc43d32305a0
roman.arzamastsev <roman.arzamastsev@russianpost.ru>
2023-01-05 18:29:26 +0300

    set liqubase and other dependencies

---
+++ src/main/java/arroal/userBase/model/user.java
@@ -0,0 +1,18 @@
+package arroal.userBase.model;
+
+public class user {
+
+    private String id;
+
+    private String phoneNumber;
+
+    private String name;
+
+    private String email;
+
+    private String address;
+
+    private String coordination;
+
+    private String comment;
+}
